8fd9b35d0e6b8ba425e41991f4d2921ecf828aac
Anshika-a11 <sanjusanju45275@gmail.com>
2026-09-07 21:52:36 +0530

    Add recentlyviewed route

--- backend/server.js
+++ backend/server.js
@@ -7,23 +7,32 @@ const productrouter = require("./routes/Productroutes");
 const Bagroutes = require("./routes/Bagroutes");
 const Wishlistroutes = require("./routes/Wishlistroutes");
 const OrderRoutes = require("./routes/OrderRoutes");
-const cors = require('cors');
+const RecentlyViewedroutes = require("./routes/RecentlyViewedroutes");
+const cors = require("cors");
+
 dotenv.config();
+
 const app = express();
+
 app.use(express.json());
+
 app.use(cors({
-  origin: '*', 
-  credentials: true, 
+  origin: "*",
+  credentials: true,
 }));
+
 app.get("/", (req, res) => {
   res.send("✅ Myntra backend in working");
 });
+
 app.use("/user", userrouter);
 app.use("/category", categoryrouter);
 app.use("/product", productrouter);
 app.use("/bag", Bagroutes);
 app.use("/wishlist", Wishlistroutes);
 app.use("/Order", OrderRoutes);
+app.use("/recently-viewed", RecentlyViewedroutes);
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -32,4 +41,5 @@ mongoose
   .catch((err) => console.log(err));
 
 const PORT = process.env.PORT;
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
